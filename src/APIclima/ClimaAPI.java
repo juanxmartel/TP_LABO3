2164748581191d0a61e3283a960d077879de47f6
juanCruz <juanx.martel@gmail.com>
2024-06-06 21:42:55 -0300

    Actualizacion

---
+++ src/APIclima/ClimaAPI.java
@@ -0,0 +1,82 @@
+package APIclima;
+
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Scanner;
+
+/**
+ * Clase encargada de gestionar toda la informacion relacionada con el clima
+ */
+public class ClimaAPI {
+    private static DatosClima datosClima;
+
+    static {
+        datosClima = new DatosClima();
+    }
+
+    /**
+     * Retorna instancia de DatosClima
+     *
+     * @return
+     */
+    public static DatosClima obtenerDatosClima() {
+        return datosClima;
+    }
+
+    /**
+     * Actualiza el atributo datosClima de la clase con la informacion nueva de la API
+     *
+     * @param fecha (yyyy-mm-dd)
+     */
+    public static void actualizarDatosClima(String fecha) {
+        try {
+            JSONObject json_datos = new JSONObject(ClimaAPI.getInfo(fecha));
+            datosClima.fromJSON(json_datos);
+        } catch (JSONException e) {
+            /// llamada recursiva a funcion
+            actualizarDatosClima(fecha);
+        }
+    }
+
+    /**
+     * Realiza peticion a la API de Clima "Open Meteo"
+     *
+     * @param fecha (yyyy-mm-dd)
+     * @return JSON en formato String
+     */
+    private static String getInfo(String fecha) {
+        System.out.println("Realizando conexion a API... ");
+
+        String apiURL = "https://archive-api.open-meteo.com/v1/archive?latitude=-38.00&longitude=-57.56" + "&start_date=" + fecha + "&end_date=" + fecha + "&hourly=temperature_2m,relativehumidity_2m,apparent_temperature,precipitation,rain,weathercode&daily=weathercode,apparent_temperature_max,apparent_temperature_min,apparent_temperature_mean&timezone=America%2FSao_Paulo";
+
+        try {
+            URL url = new URL(apiURL);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.connect();
+            int responseCode = connection.getResponseCode();
+            if (responseCode != 200) {
+                System.out.println("La API soporta fechas anteriores al 06/06/2024");
+                throw new RuntimeException("Codigo de error: " + responseCode);
+            } else {
+                StringBuilder stringBuilder = new StringBuilder();
+                Scanner scanner = new Scanner(url.openStream());
+                while (scanner.hasNext()) {
+                    stringBuilder.append(scanner.nextLine());
+                }
+                scanner.close();
+
+                return stringBuilder.toString();
+            }
+
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+        return "";
+    }
+}
--- src/Modelos/Clases/ElfoMago.java
+++ src/Modelos/Clases/ElfoMago.java
@@ -1,9 +1,16 @@
 package Modelos.Clases;
 
+import APIclima.ClimaAPI;
+import APIclima.DatosClima;
+
 import java.util.Scanner;
 
 public class ElfoMago extends Personaje {
+
+
     Scanner scanner;
+
+    DatosClima datos = new DatosClima();
 
     @Override
     public void atacar(Personaje objetivo) {
@@ -15,11 +22,69 @@ public class ElfoMago extends Personaje {
         double danio=0;
         int opcion=scanner.nextInt();
         switch (opcion){
-            case 1: danio = this.;
-            case 2: danio = this.;
+            case 1: danio = this.bolaDeFuego();
+            case 2: danio = this.impacTrueno();
+            case 3: danio = this.congelar();
+            case 4: danio = this.hidroBomba();
             default:
                 System.out.println("no elegiste bien la opcion, perdiste tu turno =) ");
         }
         objetivo.recibirDanio(danio);
+    }
+
+    public double bolaDeFuego(){
+        double danio =0;
+        if (datos.getHumedad()>80)
+        {
+            danio=20;
+            System.out.println("Gracias a la humedad tu bola de fuego hace menos daño, hizo"+danio);
+        }else {
+            danio=35;
+            System.out.println("Gracias a la humedad tu bola de fuego hace mas daño, hizo"+danio);
+
+        }
+
+
+        return danio;
+    }
+
+    public double impacTrueno(){
+        double danio=0;
+        if(datos.getHumedad()>80)
+        {
+            danio=35;
+            System.out.println("Gracias a la humedad tu rayo hace mas daño, hizo"+danio);
+
+        }else{
+            danio=25;
+            System.out.println("Gracias a la humedad tu rayo hace menos daño, hizo"+danio);
+            }
+        return danio;
+    }
+
+    public double congelar(){
+        double danio=0;
+        if(datos.getTemperaturaMaxima()<20)
+        {
+            danio=35;
+            System.out.println("Gracias a la baja temperatura tu hielo hace mas daño, hizo"+danio);
+
+        }else{
+            danio=15;
+            System.out.println("Gracias a la alta temperatura tu hielo se deshielo e hizo menos daño "+danio);
+            }
+        return danio;
+    }
+    public double hidroBomba(){
+        double danio=0;
+        if(datos.getHumedad()>80)
+        {
+            danio=35;
+            System.out.println("Aprovechas el dia humedo para hacer mas daño, hizo"+danio);
+
+        }else{danio=15;
+            System.out.println("Por culpa de la baja humedad y poco aprocechamiento del agua hizo menos daño :"+danio);
+            }
+        return danio;
     }
 }
